f57d6d0f57bb1192a24c306328c6d708c7b49da2
m-yamamoto <m-yamamoto@comedge.co.jp>
2021-12-29 21:52:24 +0900

    ユーザー情報取得処理修正

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Cake\Controller\Controller;
+use App\Controller\Condition\UsersCondition;
 use App\Repository\UsersRepository;
 
 /**
@@ -14,8 +15,11 @@ class HelloController extends Controller
     
     public function hello()
     {
-        $usersRepository = new UsersRepository();
-        // $userList = $usersRepository->select();
+        // $usersRepository = new UsersRepository();
+        // $usersCondition = new UsersCondition();
+        // $usersCondition->belongsCd = 0;
+        // $usersCondition->positionCd = 0;
+        // $userList = $usersRepository->select($usersCondition);
         // $userList = $usersRepository->insert();
         // $userList = $usersRepository->update();
         // $userList = $usersRepository->delete(2);
